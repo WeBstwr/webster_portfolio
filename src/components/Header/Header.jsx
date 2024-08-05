01649211dc92087c340e00b5b4d6386ee0fc6d13
WeBstwr <ifedhawebster@gmail.com>
2024-08-05 15:34:07 +0300

    create header
    I have created the header for the site.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import "./header.css"
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className="header">
+    <h1 className="header-logo">{`{webster}`}</h1>
+    <nav className="header-nav">
+      <a href="/" className="header-nav__link">
+        explore
+      </a>
+      <a href="/contact" className="header-nav__link">
+        contact
+      </a>
+      <button className="header__resume-btn">my resume</button>
+    </nav>
+  </header>
   )
 }
 
