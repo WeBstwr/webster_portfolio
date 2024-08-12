7b04d769c2bc8996a24fe3d886e598ff05ee4002
WeBstwr <ifedhawebster@gmail.com>
2024-08-12 12:43:42 +0300

    create footer
    I have created the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import "./footer.css";
+import today from "../../utils/date.js";
 
-function Footer() {
-  return <div>Footer</div>;
-}
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        &copy; {today()}{" "}
+        <a href="https://github.com/WeBstwr" target="_blank">
+          Webster Ifedha|
+          <a
+            href="https://www.linkedin.com/in/webster-safala-9147a2245/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+        </a>{" "}
+        All rights reserved
+      </p>
+    </div>
+  );
+};
 
 export default Footer;
