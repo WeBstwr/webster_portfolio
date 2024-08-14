949410c1c12ba33445f873c6108e7a442205a830
WeBstwr <ifedhawebster@gmail.com>
2024-08-14 14:06:37 +0300

    create contact page
    I have created the contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import Title from "../../components/Title/Title";
+import "./contact.css";
+
+const Socials = ({ icon }) => {
+  return (
+    <div className="socials">
+      <div className="socials-icon-wrapper">{icon}</div>
+    </div>
+  );
+};
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contact-section">
+      <Title text="get in touch" />
+      <div className="contact-info">
+        <h2>I am here to answer all your questions</h2>
+        <div className="contact-container">
+          <h3>email</h3>
+          <p>ifedhawebster@gmail.com</p>
+          <h3>whatsapp</h3>
+          <p>+254715652746</p>
+          <h3>time</h3>
+          <p>24/7</p>
+        </div>
+        <div className="contact-socials">
+          <Socials icon={<FaLinkedin />} />
+          <Socials icon={<FaXTwitter />} />
+          <Socials icon={<AiFillInstagram />} />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
