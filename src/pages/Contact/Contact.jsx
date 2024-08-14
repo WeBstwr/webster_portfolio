2bac50be6236f3c152dd71e26fe851024d05d8a2
WeBstwr <ifedhawebster@gmail.com>
2024-08-14 15:40:30 +0300

    update contact page
    Add the required links for my accounts

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -5,11 +5,16 @@ import { FaXTwitter } from "react-icons/fa6";
 import Title from "../../components/Title/Title";
 import "./contact.css";
 
-const Socials = ({ icon }) => {
+const Socials = ({ icon, link }) => {
   return (
-    <div className="socials">
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="socials"
+    >
       <div className="socials-icon-wrapper">{icon}</div>
-    </div>
+    </a>
   );
 };
 
@@ -18,19 +23,35 @@ function Contact() {
     <section className="contact-section">
       <Title text="get in touch" />
       <div className="contact-info">
-        <h2>I am here to answer all your questions</h2>
+        <h2>How to reach me</h2>
         <div className="contact-container">
           <h3>email</h3>
-          <p>ifedhawebster@gmail.com</p>
+          <p>
+            <a href="mailto:ifedhawebster@gmail.com">ifedhawebster@gmail.com</a>
+          </p>
           <h3>whatsapp</h3>
-          <p>+254715652746</p>
+          <p>
+            <a
+              href="https://wa.me/254715652746"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              +254715652746
+            </a>
+          </p>
           <h3>time</h3>
           <p>24/7</p>
         </div>
         <div className="contact-socials">
-          <Socials icon={<FaLinkedin />} />
-          <Socials icon={<FaXTwitter />} />
-          <Socials icon={<AiFillInstagram />} />
+          <Socials
+            icon={<FaLinkedin />}
+            link="https://www.linkedin.com/in/webster-safala-9147a2245/"
+          />
+          <Socials icon={<FaXTwitter />} link="https://x.com/WIfedha" />
+          <Socials
+            icon={<AiFillInstagram />}
+            link="https://www.instagram.com/webster_ifedha/"
+          />
         </div>
       </div>
     </section>
